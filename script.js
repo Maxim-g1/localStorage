817552d86a425ec654baad1e2c39d69d64b56678
Maxim-g1 <122662234+Maxim-g1@users.noreply.github.com>
2024-02-24 14:41:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,91 @@
-console.log(localStorage)
+// JSON.parse() - принимает строку JSON в качестве параметра и возвращает соответствующую структуру данных JavaScript
+// JSON.stringify() - принимает объект в качестве параметра и возвращает эквивалентную строковую JSON строку.
 
-//сохранение данных в LS
-localStorage.setItem('key',[100,200,300])
+// Сохранение данных в LS
+localStorage.setItem('key', [100, 200, 300]);
 
-//получить данные из LS
-let str = localStorage.getItem('key')
-console.log(typeof(str))
+// Получить данные из LS
+let str = localStorage.getItem('key');
+console.log(str);
 
-localStorage.setItem('key1',32542)
-localStorage.setItem('key2',88888)
-localStorage.setItem('key3',979797)
+// Напишите скрипт, который сохранит в локальном хранилище три числа под тремя разными ключами.
+// Запустите этот скрипт, чтобы данные сохранились.
+// Затем напишите скрипт, который получит ваши три числа из локального хранилища и найдет их сумму.
+// Запустите второй скрипт и убедитесь в его работоспособности.
 
-let key1 = +localStorage.getItem("key1")
-let key2 = +localStorage.getItem("key2")
-let key3 = +localStorage.getItem("key3")
+localStorage.setItem('a', 1488);
+localStorage.setItem('b', 200);
+localStorage.setItem('c', 300);
 
-console.log(key1+key2+key3)
+let a = Number(localStorage.getItem('a'));
+let b = Number(localStorage.getItem('b'));
+let c = Number(localStorage.getItem('c'));
 
-//однократное сохранение в LS
-let time = localStorage.getItem('time')
- 
-if(time===null){
-    let now=Date.now() /60000
-    localStorage.setItem('time', now)
-}else{
-    let count=Date.now()-time
-    console.log(`С предыдущего входа прощло ${count/60000} минут`)
-}
+let sum = a + b + c;
+console.log(sum);
+
+// Однократное сохранение в LS
+let time = localStorage.getItem('time');
+
+console.log(time);
+
+console.log(Date.now())
+
+let btnEnter = document.querySelector('#enter')
+
+btnEnter.addEventListener('click', () => {
+    if (time === null) {
+        let now = Date.now();
+        localStorage.setItem('time', now);
+    } else {
+        let now = Date.now();
+        localStorage.clear()
+        localStorage.setItem('time', now);
+        let count = Date.now() - time;
+
+        console.log(`С момента входа прошло ${count / 60000} минут`)
+
+    }
+})
+
+
+
+// По заходу пользователя на сайт запишите в локальное хранилище текущий момент времени.
+// Затем по повторному заходу выведите, сколько прошло времени с предыдущего захода пользователя на сайт.
+
+
+let keys = Object.keys(localStorage)
+console.log(keys)
+
+
+localStorage.setItem('key', 1)
+let value1 = localStorage.getItem('key')
+
+console.log('данные из LS', value1)
+
+
+localStorage.setItem('key', 2)
+value1 = localStorage.getItem('key')
+console.log('данные из LS', value1)
+
+localStorage.removeItem('key')
+console.log(localStorage.getItem('key'))
+
+console.log(localStorage.length)
+
+let key = localStorage.key(1)
+
+console.log(key)
+
+console.log(localStorage.getItem(key))
+
+let arr = [1, 2, 3, 4, 5,]
+localStorage.setItem('arr', JSON.stringify(arr))
+
+let str1 = localStorage.getItem('arr')
+let result = JSON.parse(str1)
+console.log(`${result} типа ${Array.isArray(result)}`)
+// console.log(`${str1} типа ${typeof str1}`)
+result.push(6)
+console.log(result)
+localStorage.setItem('arr', JSON.stringify(result))
